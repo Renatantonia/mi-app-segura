cbac1c7906c9dadde6a389dada0ae64b14909d9a
Renata <renata.cuello@alumnos.ucn.cl>
2025-06-19 22:06:53 -0400

    Register frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Notes from './Notes';
+import Register from './Register';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/notes" element={<Notes />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,14 +1,19 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 
 function Login() {
     const [usuario, setUsuario] = useState('');
     const [contrasena, setContrasena] = useState('');
+    const [mostrar, setMostrar] = useState(false);
     const navigate = useNavigate();
 
     const manejarClick = () => {
         navigate('/notes');
+    };
+
+    const manejarCheckbox = (e) => {
+        setMostrar(e.target.checked); // true si está tildado
     };
 
     return (
@@ -22,13 +27,18 @@ function Login() {
         />
         <br /><br />
         <input
-            type="password"
+            type={mostrar ? 'text' : 'password'}
             placeholder="Contraseña"
             value={contrasena}
             onChange={(e) => setContrasena(e.target.value)}
         />
+        <label>
+            <input type="checkbox" checked={mostrar} onChange={manejarCheckbox} />
+        </label>
         <br /><br />
         <button onClick={manejarClick}>Iniciar Sesión</button>
+        <br /><br />
+        <span>¿No tienes cuenta? <Link to="/register">¡Regístrate aquí!</Link></span>
         </div>
     );
 }
